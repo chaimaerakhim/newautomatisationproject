e33df3c5e17ecc075a35b2b7870be7ae6b27a2f2
chaimaerakhim <chaimaarakhime@gmail.com>
2018-07-06 11:14:07 +0100

    Initial commit

---
+++ src/main/java/com/neoxia/newautomatisationproject/NewautomatisationprojectApplication.java
@@ -0,0 +1,12 @@
+package com.neoxia.newautomatisationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewautomatisationprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewautomatisationprojectApplication.class, args);
+    }
+}
